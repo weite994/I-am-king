CloudEngineHub/github-mcp-server

Add UpdateGist tool

pkg/github/gists.go
@@ -165,3 +165,83 @@ func CreateGist(getClient GetClientFn, t translations.TranslationHelperFunc) (to
 			return mcp.NewToolResultText(string(r)), nil
 		}
 }
+
+// UpdateGist creates a tool to edit an existing gist
+func UpdateGist(getClient GetClientFn, t translations.TranslationHelperFunc) (tool mcp.Tool, handler server.ToolHandlerFunc) {
+	return mcp.NewTool("update_gist",
+			mcp.WithDescription(t("TOOL_UPDATE_GIST_DESCRIPTION", "Update an existing gist")),
+			mcp.WithString("gist_id",
+				mcp.Required(),
+				mcp.Description("ID of the gist to update"),
+			),
+			mcp.WithString("description",
+				mcp.Description("Updated description of the gist"),
+			),
+			mcp.WithString("filename",
+				mcp.Required(),
+				mcp.Description("Filename to update or create"),
+			),
+			mcp.WithString("content",
+				mcp.Required(),
+				mcp.Description("Content for the file"),
+			),
+		),
+		func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+			gistID, err := requiredParam[string](request, "gist_id")
+			if err != nil {
+				return mcp.NewToolResultError(err.Error()), nil
+			}
+
+			description, err := OptionalParam[string](request, "description")
+			if err != nil {
+				return mcp.NewToolResultError(err.Error()), nil
+			}
+
+			filename, err := requiredParam[string](request, "filename")
+			if err != nil {
+				return mcp.NewToolResultError(err.Error()), nil
+			}
+
+			content, err := requiredParam[string](request, "content")
+			if err != nil {
+				return mcp.NewToolResultError(err.Error()), nil
+			}
+
+			files := make(map[github.GistFilename]github.GistFile)
+			files[github.GistFilename(filename)] = github.GistFile{
+				Filename: github.Ptr(filename),
+				Content:  github.Ptr(content),
+			}
+
+			gist := &github.Gist{
+				Files:       files,
+				Description: github.Ptr(description),
+			}
+
+			client, err := getClient(ctx)
+			if err != nil {
+				return nil, fmt.Errorf("failed to get GitHub client: %w", err)
+			}
+
+			updatedGist, resp, err := client.Gists.Edit(ctx, gistID, gist)
+			if err != nil {
+				return nil, fmt.Errorf("failed to update gist: %w", err)
+			}
+			defer func() { _ = resp.Body.Close() }()
+
+			if resp.StatusCode != http.StatusOK {
+				body, err := io.ReadAll(resp.Body)
+				if err != nil {
+					return nil, fmt.Errorf("failed to read response body: %w", err)
+				}
+				return mcp.NewToolResultError(fmt.Sprintf("failed to update gist: %s", string(body))), nil
+			}
+
+			r, err := json.Marshal(updatedGist)
+			if err != nil {
+				return nil, fmt.Errorf("failed to marshal response: %w", err)
+			}
+
+			return mcp.NewToolResultText(string(r)), nil
+		}
+}

weite994/I-am-king

pkg/github/gists.go
@@ -165,3 +165,83 @@ func CreateGist(getClient GetClientFn, t translations.TranslationHelperFunc) (to
 			return mcp.NewToolResultText(string(r)), nil
 		}
 }
+
+// UpdateGist creates a tool to edit an existing gist
+func UpdateGist(getClient GetClientFn, t translations.TranslationHelperFunc) (tool mcp.Tool, handler server.ToolHandlerFunc) {
+	return mcp.NewTool("update_gist",
+			mcp.WithDescription(t("TOOL_UPDATE_GIST_DESCRIPTION", "Update an existing gist")),
+			mcp.WithString("gist_id",
+				mcp.Required(),
+				mcp.Description("ID of the gist to update"),
+			),
+			mcp.WithString("description",
+				mcp.Description("Updated description of the gist"),
+			),
+			mcp.WithString("filename",
+				mcp.Required(),
+				mcp.Description("Filename to update or create"),
+			),
+			mcp.WithString("content",
+				mcp.Required(),
+				mcp.Description("Content for the file"),
+			),
+		),
+		func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+			gistID, err := RequiredParam[string](request, "gist_id")
+			if err != nil {
+				return mcp.NewToolResultError(err.Error()), nil
+			}
+
+			description, err := OptionalParam[string](request, "description")
+			if err != nil {
+				return mcp.NewToolResultError(err.Error()), nil
+			}
+
+			filename, err := RequiredParam[string](request, "filename")
+			if err != nil {
+				return mcp.NewToolResultError(err.Error()), nil
+			}
+
+			content, err := RequiredParam[string](request, "content")
+			if err != nil {
+				return mcp.NewToolResultError(err.Error()), nil
+			}
+
+			files := make(map[github.GistFilename]github.GistFile)
+			files[github.GistFilename(filename)] = github.GistFile{
+				Filename: github.Ptr(filename),
+				Content:  github.Ptr(content),
+			}
+
+			gist := &github.Gist{
+				Files:       files,
+				Description: github.Ptr(description),
+			}
+
+			client, err := getClient(ctx)
+			if err != nil {
+				return nil, fmt.Errorf("failed to get GitHub client: %w", err)
+			}
+
+			updatedGist, resp, err := client.Gists.Edit(ctx, gistID, gist)
+			if err != nil {
+				return nil, fmt.Errorf("failed to update gist: %w", err)
+			}
+			defer func() { _ = resp.Body.Close() }()
+
+			if resp.StatusCode != http.StatusOK {
+				body, err := io.ReadAll(resp.Body)
+				if err != nil {
+					return nil, fmt.Errorf("failed to read response body: %w", err)
+				}
+				return mcp.NewToolResultError(fmt.Sprintf("failed to update gist: %s", string(body))), nil
+			}
+
+			r, err := json.Marshal(updatedGist)
+			if err != nil {
+				return nil, fmt.Errorf("failed to marshal response: %w", err)
+			}
+
+			return mcp.NewToolResultText(string(r)), nil
+		}
+}