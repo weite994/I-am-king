yonaka15/github-mcp-server

Add Global Security Advisories Toolset

pkg/github/security_advisories_test.go
@@ -1,0 +1,241 @@
+package github
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/github/github-mcp-server/pkg/translations"
+	"github.com/google/go-github/v72/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_ListGlobalSecurityAdvisories(t *testing.T) {
+	mockClient := github.NewClient(nil)
+	tool, _ := ListGlobalSecurityAdvisories(stubGetClientFn(mockClient), translations.NullTranslationHelper)
+
+	assert.Equal(t, "list_global_security_advisories", tool.Name)
+	assert.NotEmpty(t, tool.Description)
+	assert.Contains(t, tool.InputSchema.Properties, "ecosystem")
+	assert.Contains(t, tool.InputSchema.Properties, "severity")
+	assert.Contains(t, tool.InputSchema.Properties, "ghsaId")
+	assert.ElementsMatch(t, tool.InputSchema.Required, []string{})
+
+	// Setup mock advisory for success case
+	mockAdvisory := &github.GlobalSecurityAdvisory{
+		SecurityAdvisory: github.SecurityAdvisory{
+			GHSAID:      github.Ptr("GHSA-xxxx-xxxx-xxxx"),
+			Summary:     github.Ptr("Test advisory"),
+			Description: github.Ptr("This is a test advisory."),
+			Severity:    github.Ptr("high"),
+		},
+	}
+
+	tests := []struct {
+		name               string
+		mockedClient       *http.Client
+		requestArgs        map[string]interface{}
+		expectError        bool
+		expectedAdvisories []*github.GlobalSecurityAdvisory
+		expectedErrMsg     string
+	}{
+		{
+			name: "successful advisory fetch",
+			mockedClient: mock.NewMockedHTTPClient(
+				mock.WithRequestMatch(
+					mock.GetAdvisories,
+					[]*github.GlobalSecurityAdvisory{mockAdvisory},
+				),
+			),
+			requestArgs: map[string]interface{}{
+				"ecosystem": "npm",
+				"severity":  "high",
+			},
+			expectError:        false,
+			expectedAdvisories: []*github.GlobalSecurityAdvisory{mockAdvisory},
+		},
+		{
+			name: "invalid severity value",
+			mockedClient: mock.NewMockedHTTPClient(
+				mock.WithRequestMatchHandler(
+					mock.GetAdvisories,
+					http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+						w.WriteHeader(http.StatusBadRequest)
+						_, _ = w.Write([]byte(`{"message": "Bad Request"}`))
+					}),
+				),
+			),
+			requestArgs: map[string]interface{}{
+				"severity": "extreme",
+			},
+			expectError:    true,
+			expectedErrMsg: "failed to list global security advisories",
+		},
+		{
+			name: "API error handling",
+			mockedClient: mock.NewMockedHTTPClient(
+				mock.WithRequestMatchHandler(
+					mock.GetAdvisories,
+					http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+						w.WriteHeader(http.StatusInternalServerError)
+						_, _ = w.Write([]byte(`{"message": "Internal Server Error"}`))
+					}),
+				),
+			),
+			requestArgs:    map[string]interface{}{},
+			expectError:    true,
+			expectedErrMsg: "failed to list global security advisories",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// Setup client with mock
+			client := github.NewClient(tc.mockedClient)
+			_, handler := ListGlobalSecurityAdvisories(stubGetClientFn(client), translations.NullTranslationHelper)
+
+			// Create call request
+			request := createMCPRequest(tc.requestArgs)
+
+			// Call handler
+			result, err := handler(context.Background(), request)
+
+			// Verify results
+			if tc.expectError {
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), tc.expectedErrMsg)
+				return
+			}
+
+			require.NoError(t, err)
+
+			// Parse the result and get the text content if no error
+			textContent := getTextResult(t, result)
+
+			// Unmarshal and verify the result
+			var returnedAdvisories []*github.GlobalSecurityAdvisory
+			err = json.Unmarshal([]byte(textContent.Text), &returnedAdvisories)
+			assert.NoError(t, err)
+			assert.Len(t, returnedAdvisories, len(tc.expectedAdvisories))
+			for i, advisory := range returnedAdvisories {
+				assert.Equal(t, *tc.expectedAdvisories[i].GHSAID, *advisory.GHSAID)
+				assert.Equal(t, *tc.expectedAdvisories[i].Summary, *advisory.Summary)
+				assert.Equal(t, *tc.expectedAdvisories[i].Description, *advisory.Description)
+				assert.Equal(t, *tc.expectedAdvisories[i].Severity, *advisory.Severity)
+			}
+		})
+	}
+}
+
+func Test_GetGlobalSecurityAdvisory(t *testing.T) {
+	mockClient := github.NewClient(nil)
+	tool, _ := GetGlobalSecurityAdvisory(stubGetClientFn(mockClient), translations.NullTranslationHelper)
+
+	assert.Equal(t, "get_global_security_advisory", tool.Name)
+	assert.NotEmpty(t, tool.Description)
+	assert.Contains(t, tool.InputSchema.Properties, "ghsaId")
+	assert.ElementsMatch(t, tool.InputSchema.Required, []string{"ghsaId"})
+
+	// Setup mock advisory for success case
+	mockAdvisory := &github.GlobalSecurityAdvisory{
+		SecurityAdvisory: github.SecurityAdvisory{
+			GHSAID:      github.Ptr("GHSA-xxxx-xxxx-xxxx"),
+			Summary:     github.Ptr("Test advisory"),
+			Description: github.Ptr("This is a test advisory."),
+			Severity:    github.Ptr("high"),
+		},
+	}
+
+	tests := []struct {
+		name             string
+		mockedClient     *http.Client
+		requestArgs      map[string]interface{}
+		expectError      bool
+		expectedAdvisory *github.GlobalSecurityAdvisory
+		expectedErrMsg   string
+	}{
+		{
+			name: "successful advisory fetch",
+			mockedClient: mock.NewMockedHTTPClient(
+				mock.WithRequestMatch(
+					mock.GetAdvisoriesByGhsaId,
+					mockAdvisory,
+				),
+			),
+			requestArgs: map[string]interface{}{
+				"ghsaId": "GHSA-xxxx-xxxx-xxxx",
+			},
+			expectError:      false,
+			expectedAdvisory: mockAdvisory,
+		},
+		{
+			name: "invalid ghsaId format",
+			mockedClient: mock.NewMockedHTTPClient(
+				mock.WithRequestMatchHandler(
+					mock.GetAdvisoriesByGhsaId,
+					http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+						w.WriteHeader(http.StatusBadRequest)
+						_, _ = w.Write([]byte(`{"message": "Bad Request"}`))
+					}),
+				),
+			),
+			requestArgs: map[string]interface{}{
+				"ghsaId": "invalid-ghsa-id",
+			},
+			expectError:    true,
+			expectedErrMsg: "failed to get advisory",
+		},
+		{
+			name: "advisory not found",
+			mockedClient: mock.NewMockedHTTPClient(
+				mock.WithRequestMatchHandler(
+					mock.GetAdvisoriesByGhsaId,
+					http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+						w.WriteHeader(http.StatusNotFound)
+						_, _ = w.Write([]byte(`{"message": "Not Found"}`))
+					}),
+				),
+			),
+			requestArgs: map[string]interface{}{
+				"ghsaId": "GHSA-xxxx-xxxx-xxxx",
+			},
+			expectError:    true,
+			expectedErrMsg: "failed to get advisory",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// Setup client with mock
+			client := github.NewClient(tc.mockedClient)
+			_, handler := GetGlobalSecurityAdvisory(stubGetClientFn(client), translations.NullTranslationHelper)
+
+			// Create call request
+			request := createMCPRequest(tc.requestArgs)
+
+			// Call handler
+			result, err := handler(context.Background(), request)
+
+			// Verify results
+			if tc.expectError {
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), tc.expectedErrMsg)
+				return
+			}
+
+			require.NoError(t, err)
+
+			// Parse the result and get the text content if no error
+			textContent := getTextResult(t, result)
+
+			// Verify the result
+			assert.Contains(t, textContent.Text, *tc.expectedAdvisory.GHSAID)
+			assert.Contains(t, textContent.Text, *tc.expectedAdvisory.Summary)
+			assert.Contains(t, textContent.Text, *tc.expectedAdvisory.Description)
+			assert.Contains(t, textContent.Text, *tc.expectedAdvisory.Severity)
+		})
+	}
+}

weite994/I-am-king

pkg/github/security_advisories_test.go
@@ -1,0 +1,243 @@
+package github
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/github/github-mcp-server/pkg/translations"
+	"github.com/google/go-github/v74/github"
+	"github.com/migueleliasweb/go-github-mock/src/mock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_ListGlobalSecurityAdvisories(t *testing.T) {
+	mockClient := github.NewClient(nil)
+	tool, _ := ListGlobalSecurityAdvisories(stubGetClientFn(mockClient), translations.NullTranslationHelper)
+
+	assert.Equal(t, "list_global_security_advisories", tool.Name)
+	assert.NotEmpty(t, tool.Description)
+	assert.Contains(t, tool.InputSchema.Properties, "ecosystem")
+	assert.Contains(t, tool.InputSchema.Properties, "severity")
+	assert.Contains(t, tool.InputSchema.Properties, "ghsaId")
+	assert.ElementsMatch(t, tool.InputSchema.Required, []string{})
+
+	// Setup mock advisory for success case
+	mockAdvisory := &github.GlobalSecurityAdvisory{
+		SecurityAdvisory: github.SecurityAdvisory{
+			GHSAID:      github.Ptr("GHSA-xxxx-xxxx-xxxx"),
+			Summary:     github.Ptr("Test advisory"),
+			Description: github.Ptr("This is a test advisory."),
+			Severity:    github.Ptr("high"),
+		},
+	}
+
+	tests := []struct {
+		name               string
+		mockedClient       *http.Client
+		requestArgs        map[string]interface{}
+		expectError        bool
+		expectedAdvisories []*github.GlobalSecurityAdvisory
+		expectedErrMsg     string
+	}{
+		{
+			name: "successful advisory fetch",
+			mockedClient: mock.NewMockedHTTPClient(
+				mock.WithRequestMatch(
+					mock.GetAdvisories,
+					[]*github.GlobalSecurityAdvisory{mockAdvisory},
+				),
+			),
+			requestArgs: map[string]interface{}{
+				"type":      "reviewed",
+				"ecosystem": "npm",
+				"severity":  "high",
+			},
+			expectError:        false,
+			expectedAdvisories: []*github.GlobalSecurityAdvisory{mockAdvisory},
+		},
+		{
+			name: "invalid severity value",
+			mockedClient: mock.NewMockedHTTPClient(
+				mock.WithRequestMatchHandler(
+					mock.GetAdvisories,
+					http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+						w.WriteHeader(http.StatusBadRequest)
+						_, _ = w.Write([]byte(`{"message": "Bad Request"}`))
+					}),
+				),
+			),
+			requestArgs: map[string]interface{}{
+				"type":     "reviewed",
+				"severity": "extreme",
+			},
+			expectError:    true,
+			expectedErrMsg: "failed to list global security advisories",
+		},
+		{
+			name: "API error handling",
+			mockedClient: mock.NewMockedHTTPClient(
+				mock.WithRequestMatchHandler(
+					mock.GetAdvisories,
+					http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+						w.WriteHeader(http.StatusInternalServerError)
+						_, _ = w.Write([]byte(`{"message": "Internal Server Error"}`))
+					}),
+				),
+			),
+			requestArgs:    map[string]interface{}{},
+			expectError:    true,
+			expectedErrMsg: "failed to list global security advisories",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// Setup client with mock
+			client := github.NewClient(tc.mockedClient)
+			_, handler := ListGlobalSecurityAdvisories(stubGetClientFn(client), translations.NullTranslationHelper)
+
+			// Create call request
+			request := createMCPRequest(tc.requestArgs)
+
+			// Call handler
+			result, err := handler(context.Background(), request)
+
+			// Verify results
+			if tc.expectError {
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), tc.expectedErrMsg)
+				return
+			}
+
+			require.NoError(t, err)
+
+			// Parse the result and get the text content if no error
+			textContent := getTextResult(t, result)
+
+			// Unmarshal and verify the result
+			var returnedAdvisories []*github.GlobalSecurityAdvisory
+			err = json.Unmarshal([]byte(textContent.Text), &returnedAdvisories)
+			assert.NoError(t, err)
+			assert.Len(t, returnedAdvisories, len(tc.expectedAdvisories))
+			for i, advisory := range returnedAdvisories {
+				assert.Equal(t, *tc.expectedAdvisories[i].GHSAID, *advisory.GHSAID)
+				assert.Equal(t, *tc.expectedAdvisories[i].Summary, *advisory.Summary)
+				assert.Equal(t, *tc.expectedAdvisories[i].Description, *advisory.Description)
+				assert.Equal(t, *tc.expectedAdvisories[i].Severity, *advisory.Severity)
+			}
+		})
+	}
+}
+
+func Test_GetGlobalSecurityAdvisory(t *testing.T) {
+	mockClient := github.NewClient(nil)
+	tool, _ := GetGlobalSecurityAdvisory(stubGetClientFn(mockClient), translations.NullTranslationHelper)
+
+	assert.Equal(t, "get_global_security_advisory", tool.Name)
+	assert.NotEmpty(t, tool.Description)
+	assert.Contains(t, tool.InputSchema.Properties, "ghsaId")
+	assert.ElementsMatch(t, tool.InputSchema.Required, []string{"ghsaId"})
+
+	// Setup mock advisory for success case
+	mockAdvisory := &github.GlobalSecurityAdvisory{
+		SecurityAdvisory: github.SecurityAdvisory{
+			GHSAID:      github.Ptr("GHSA-xxxx-xxxx-xxxx"),
+			Summary:     github.Ptr("Test advisory"),
+			Description: github.Ptr("This is a test advisory."),
+			Severity:    github.Ptr("high"),
+		},
+	}
+
+	tests := []struct {
+		name             string
+		mockedClient     *http.Client
+		requestArgs      map[string]interface{}
+		expectError      bool
+		expectedAdvisory *github.GlobalSecurityAdvisory
+		expectedErrMsg   string
+	}{
+		{
+			name: "successful advisory fetch",
+			mockedClient: mock.NewMockedHTTPClient(
+				mock.WithRequestMatch(
+					mock.GetAdvisoriesByGhsaId,
+					mockAdvisory,
+				),
+			),
+			requestArgs: map[string]interface{}{
+				"ghsaId": "GHSA-xxxx-xxxx-xxxx",
+			},
+			expectError:      false,
+			expectedAdvisory: mockAdvisory,
+		},
+		{
+			name: "invalid ghsaId format",
+			mockedClient: mock.NewMockedHTTPClient(
+				mock.WithRequestMatchHandler(
+					mock.GetAdvisoriesByGhsaId,
+					http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+						w.WriteHeader(http.StatusBadRequest)
+						_, _ = w.Write([]byte(`{"message": "Bad Request"}`))
+					}),
+				),
+			),
+			requestArgs: map[string]interface{}{
+				"ghsaId": "invalid-ghsa-id",
+			},
+			expectError:    true,
+			expectedErrMsg: "failed to get advisory",
+		},
+		{
+			name: "advisory not found",
+			mockedClient: mock.NewMockedHTTPClient(
+				mock.WithRequestMatchHandler(
+					mock.GetAdvisoriesByGhsaId,
+					http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+						w.WriteHeader(http.StatusNotFound)
+						_, _ = w.Write([]byte(`{"message": "Not Found"}`))
+					}),
+				),
+			),
+			requestArgs: map[string]interface{}{
+				"ghsaId": "GHSA-xxxx-xxxx-xxxx",
+			},
+			expectError:    true,
+			expectedErrMsg: "failed to get advisory",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			// Setup client with mock
+			client := github.NewClient(tc.mockedClient)
+			_, handler := GetGlobalSecurityAdvisory(stubGetClientFn(client), translations.NullTranslationHelper)
+
+			// Create call request
+			request := createMCPRequest(tc.requestArgs)
+
+			// Call handler
+			result, err := handler(context.Background(), request)
+
+			// Verify results
+			if tc.expectError {
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), tc.expectedErrMsg)
+				return
+			}
+
+			require.NoError(t, err)
+
+			// Parse the result and get the text content if no error
+			textContent := getTextResult(t, result)
+
+			// Verify the result
+			assert.Contains(t, textContent.Text, *tc.expectedAdvisory.GHSAID)
+			assert.Contains(t, textContent.Text, *tc.expectedAdvisory.Summary)
+			assert.Contains(t, textContent.Text, *tc.expectedAdvisory.Description)
+			assert.Contains(t, textContent.Text, *tc.expectedAdvisory.Severity)
+		})
+	}
+}